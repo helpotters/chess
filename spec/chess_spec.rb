dfe803491bbd5574510aefff45f679deb257628b
Paul Lemus <paullemus@protonmail.com>
2021-10-30 17:16:25 -0700

    Initialized Project

---
+++ spec/chess_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Chess do
+  it "has a version number" do
+    expect(Chess::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
