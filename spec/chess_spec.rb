7ebc71393955195dc913113eb29def8fb8e7a6b7
Paul Lemus <paullemus@protonmail.com>
2021-11-06 19:37:13 -0700

    Updated Raise Exception to Raise NotationError
    Tests still pass, but Exception is too general to be useful.
    NotationError was made specifically for this use-case.

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -6,7 +6,6 @@ include Pieces
 RSpec.describe Player do
   let(:player) { ChessGame::Player.new }
   context "create pieces" do
-    # NOTE: Could remove dependence on Pieces by using mock responses
     subject(:player_pieces) { player.instance_variable_get(:@pieces).to_a }
     it { is_expected.to include(an_instance_of(Pawn)).exactly(8).times }
     it { is_expected.to include(an_instance_of(Bishop)).twice }
@@ -27,9 +26,9 @@ RSpec.describe Player do
           expect(player.input("Nb4")).to include({ piece: "N" })
         end
         it "should only allow one piece per entry" do
-          expect { player.input("NNc4") }.to raise_error(Exception)
-          expect { player.input(" ") }.to raise_error(Exception)
-          expect { player.input("Knight to j6") }.to raise_error(Exception)
+          expect { player.input("NNc4") }.to raise_error(NotationError)
+          expect { player.input(" ") }.to raise_error(NotationError)
+          expect { player.input("Knight to j6") }.to raise_error(NotationError)
         end
         it "should return both a piece and/or location hash" do
           location_hash = { piece: "N", location: ["a", 4] }
