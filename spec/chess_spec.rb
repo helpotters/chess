c0948525f376e3d1c508b47d468348c9e14c7c26
Paul Lemus <paullemus@protonmail.com>
2021-12-09 15:53:14 -0800

    Added Empty Tests for Board Representation

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe Chess do
-  it "has a version number" do
-    expect(Chess::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  xdescribe "Board Representation" do
+    xcontext "should create a board from an 8x8 nested array" do
+      xit "should be accessible via row/column values"
+      xit "should be empty"
+    end
+    xcontext "should contain information about the environment" do
+      xcontext "metadata" do
+        xit "should contain information about what piece is occupying a space"
+        xit "should expect find side assignment for any occupying piece"
+        xit "should receive positional updates from pieces"
+        xit "should receive information about the paths a piece can move in"
+        xit "should receive info about pieces attacking or defending a position"
+      end
+    end
   end
 end
