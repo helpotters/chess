bbf71101f21f2ff5ef0a322627e0a976763af3f6
Paul Lemus <paullemus@protonmail.com>
2022-01-19 21:37:40 -0800

    fix: Merge Conflict

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-RSpec.describe Chess do
-  xdescribe "Board Representation" do
-    xcontext "should create a board from an 8x8 nested array" do
-      xit "should be accessible via row/column values"
-      xit "should be empty"
-    end
-    xcontext "should contain information about the environment" do
-      xcontext "metadata" do
-        xit "should contain information about what piece is occupying a space"
-        xit "should expect find side assignment for any occupying piece"
-        xit "should receive positional updates from pieces"
-        xit "should receive information about the paths a piece can move in"
-        xit "should receive info about pieces attacking or defending a position"
-=======
 require "chess"
 
 RSpec.describe Chess do
@@ -28,7 +13,6 @@ RSpec.describe Board do
       expect(board_info.length).to eq(8)
       board_info.each do |row|
         expect(row).to be_an_instance_of(Array)
->>>>>>> 108f345 (test: Initialize 8x8 Board Array)
       end
     end
   end
