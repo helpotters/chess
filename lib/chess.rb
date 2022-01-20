b60c7c7ae0378abc2e66eeef43e1279eb7a45ead
Paul Lemus <paullemus@protonmail.com>
2022-01-19 21:31:16 -0800

    test: Initialized 8x8 Board Array
    The chessboard is represented by a nested array. Each array (row)
    contains 8 column arrays.

--- lib/chess.rb
+++ lib/chess.rb
@@ -4,5 +4,14 @@ require_relative "chess/version"
 
 module Chess
   class Error < StandardError; end
+
   # Your code goes here...
+end
+
+class Board
+  attr_reader :board_info
+
+  def initialize
+    @board_info = Array.new(8, [])
+  end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+<<<<<<< HEAD
 RSpec.describe Chess do
   xdescribe "Board Representation" do
     xcontext "should create a board from an 8x8 nested array" do
@@ -13,6 +14,21 @@ RSpec.describe Chess do
         xit "should receive positional updates from pieces"
         xit "should receive information about the paths a piece can move in"
         xit "should receive info about pieces attacking or defending a position"
+=======
+require "chess"
+
+RSpec.describe Chess do
+end
+RSpec.describe Board do
+  context "Board Representation" do
+    subject(:chessboard) { Board.new }
+    subject(:board_info) { chessboard.board_info }
+    it "should initialize an 8x8 nested array" do
+      expect(board_info).to be_an_instance_of(Array)
+      expect(board_info.length).to eq(8)
+      board_info.each do |row|
+        expect(row).to be_an_instance_of(Array)
+>>>>>>> 108f345 (test: Initialize 8x8 Board Array)
       end
     end
   end
